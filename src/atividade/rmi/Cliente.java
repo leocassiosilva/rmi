1dd1403cd3f7834899e5970b0ad5f9b6848bbff4
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-07 14:22:34 -0300

    Subindo

--- src/atividade/rmi/Cliente.java
+++ src/atividade/rmi/Cliente.java
@@ -3,6 +3,7 @@ package atividade.rmi;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,14 +18,59 @@ public class Cliente {
             ArrayList<Produto> produtos = new ArrayList();
             Registry registry = LocateRegistry.getRegistry("localhost", 9999);
             IHello obj = (IHello) registry.lookup("HelloService");
-            Produto prod = new Produto("Leocassio", 1000);
-            System.out.println(obj.Adicionar(prod));
-            produtos = obj.Mostrar();
-            for (Produto p : produtos) {
-                System.out.println(p.getNome());
+            Scanner leitura = new Scanner(System.in);
+            boolean laco = true;
+            int op;
+            while (laco) {
+                System.out.println("-----------Loja Deu Certo-----------");
+                System.out.println("1 - Inserir produto");
+                System.out.println("2 - Listar produtos");
+                System.out.println("3 - Remover produto");
+                System.out.println("4 - Buscar produto");
+                System.out.println("5 - Sair");
+
+                System.out.println("Entre com sua opção: ");
+                op = Integer.parseInt(leitura.nextLine());
+                Produto prod;
+
+                String nome;
+                float preco;
+                switch (op) {
+
+                    case 1:
+                        System.out.println("Entre com o nome do produto:");
+                        nome = leitura.nextLine();
+
+                        System.out.println("Entre com o valor do produto: ");
+                        preco = Float.parseFloat(leitura.nextLine());
+                        prod = new Produto(nome, preco);
+                        System.out.println(obj.Adicionar(prod));
+                        break;
+
+                    case 2:
+                        produtos = obj.Mostrar();
+                        for (Produto p : produtos) {
+                            System.out.println(p.getNome());
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Entre com o nome do produto:");
+                        nome = leitura.nextLine();
+                        System.out.println(obj.Remover("Leocassio"));
+                        break;
+                    case 4:
+                        System.out.println("Entre com o nome do produto:");
+                        nome = leitura.nextLine();
+                        System.out.println(obj.Pesquisar(nome));
+                        break;
+                    case 5:
+                        System.exit(0);
+                        break;
+                    default:
+                    System.out.println("Essa opção não existe!");
+
+                }
             }
-            System.out.println(obj.Pesquisar("Leocassio"));
-            System.out.println(obj.Remover("Leocassio"));
         } catch (Exception ex) {
             Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
         }
